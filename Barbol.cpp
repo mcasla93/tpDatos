6fd24922eb98217997f74243ef312acd70819d3a
pabliyo <pablofeliz1989@gmail.com>
2016-05-31 00:42:35 -0300

    nueva clase para ir modificando y sacar template

---
+++ Barbol.cpp
@@ -0,0 +1,21 @@
+/*
+ * Barbol.cpp
+ *
+ *  Created on: 31/05/2016
+ *      Author: pablo
+ */
+
+#include "Barbol.h"
+
+namespace std {
+
+Barbol::Barbol() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Barbol::~Barbol() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Barbol.h
@@ -0,0 +1,21 @@
+/*
+ * Barbol.h
+ *
+ *  Created on: 31/05/2016
+ *      Author: pablo
+ */
+
+#ifndef BARBOL_H_
+#define BARBOL_H_
+
+namespace std {
+
+class Barbol {
+public:
+	Barbol();
+	virtual ~Barbol();
+};
+
+} /* namespace std */
+
+#endif /* BARBOL_H_ */
