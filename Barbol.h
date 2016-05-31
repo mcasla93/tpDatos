1dbfeda251b8309d8464ce028997820218371057
pabliyo <pablofeliz1989@gmail.com>
2016-05-31 01:35:26 -0300

    nueva clase para ir preparado

--- Barbol.h
+++ Barbol.h
@@ -9,13 +9,47 @@
 #define BARBOL_H_
 
 namespace std {
+#include <iostream>
+#include <cstddef>
+#include <cstdlib>
 
-class Barbol {
-public:
-	Barbol();
-	virtual ~Barbol();
+using namespace std;
+/*
+class Btree {
+
+protected: // dato members
+	B_node<Type, order> *root;
+private:
+	void recInorder(B_node<Type, order> * current );
+	bool  Encontrar( B_node<Type, order> *current, Type &target);
+
+public: // publics.
+	Btree();
+	// travesling
+	void inOrder();
+	bool buscar( Type &searchitem );
+	void mostrarArbol();
+	// insertion
+	bool buscarEnNodo( B_node<Type, order> *current, const Type &target, int &position );
+	void insertar(const Type &new_entry);
+	void insertarEnRecursion( B_node<Type, order> *current, const Type &new_entry,Type &median,B_node<Type, order> * &rightchilds , bool &result );
+	void insertarEnNodo(B_node<Type, order> *current, const Type &entry, B_node<Type, order> *rightchilds, int position);
+	void dividirNodo( B_node<Type, order> *current,  const Type &extra_entry,   B_node<Type, order> *extra_childs,  int position,
+		B_node<Type, order> * &right_half,   Type &median);
+	// eliminacion
+
+	void remover( const Type &target );
+	void removerEnRecursion( B_node<Type, order> *current, const Type &target );
+	void removerDato(  B_node<Type, order> *current, int position  );
+	void copiarDePredecesor(  B_node<Type, order> *current, int position );
+	// funciones para restaurar despues de la eliminacion
+	void restaurar( B_node<Type, order> *current ,int position  );
+	void moverAlaIzquierda( B_node<Type, order> *current , int position );
+	void moverAlaDerecha( B_node<Type, order> *current , int position );
+	void combinar( B_node<Type, order> *current, int position );
+
+	void imprimirArbol( B_node<Type, order> *current );
 };
-
-} /* namespace std */
+*/
 
 #endif /* BARBOL_H_ */
